5179dfe4bbb01cd5737f5a10bf361fd0016fd5bd
sam-kim99 <samkim0451@gmail.com>
2024-06-12 12:35:35 -0400

    remove localhost from createConsumer

--- frontend/utils/consumer.js
+++ frontend/utils/consumer.js
@@ -1,5 +1,5 @@
-import {createConsumer} from '@rails/actioncable'
+import {createConsumer} from '@rails/actioncable';
 
-const consumer = createConsumer('ws://localhost:3000/cable')
+const consumer = createConsumer();
 
 export default consumer;
